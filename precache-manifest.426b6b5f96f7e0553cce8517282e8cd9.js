e44d17773601ab86de9ee4d449d47e25c12278b4
Alvin J. Alexander <becafil816@trufilth.com>
2020-11-19 03:05:26 -0600

    Updates

---
+++ precache-manifest.426b6b5f96f7e0553cce8517282e8cd9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb4288e28af3c85ea4785427eeb71c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0e269d48997c1185e9",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "116b8fdaa5fce39e7584",
+    "url": "/static/css/main.69fafb29.chunk.css"
+  },
+  {
+    "revision": "1e0e269d48997c1185e9",
+    "url": "/static/js/2.8bad3a87.chunk.js"
+  },
+  {
+    "revision": "9870115ff28e3add0b491ab406a81797",
+    "url": "/static/js/2.8bad3a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116b8fdaa5fce39e7584",
+    "url": "/static/js/main.5ac2f2e5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2e16e85ae5db92bbd48913133c5ce0ec",
+    "url": "/static/media/black.2e16e85a.png"
+  },
+  {
+    "revision": "6606b6197571b1d6155b3d85dd7f3e06",
+    "url": "/static/media/telegramLogo.6606b619.png"
+  },
+  {
+    "revision": "b730ba9fb8daa02b93dc45f78300dc11",
+    "url": "/static/media/twitterLogo.b730ba9f.png"
+  }
+]);
